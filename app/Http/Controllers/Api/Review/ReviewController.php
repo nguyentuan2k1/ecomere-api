a9113829a950df6cb8da1c9cba8777ee7984f944
ThanhDuy <tduy24699@gmail.com>
2024-03-20 00:57:49 +0700

    fix empty content request

--- app/Http/Controllers/Api/Review/ReviewController.php
+++ app/Http/Controllers/Api/Review/ReviewController.php
@@ -116,6 +116,7 @@ class ReviewController extends BaseController
             $data['user_id']    = $user->id;
             $data['product_id'] = $request->get("product_id");
             $data['rating']     = $request->get("rating");
+            $data['content']    = $request->get("content") ?? "";
 
             if (empty($data['product_id'])) return $this->sendError("Product id is required", 400);
 
@@ -132,8 +133,6 @@ class ReviewController extends BaseController
             if (intval($data['rating']) < 1) return $this->sendError("Rating must be positive number", 400);
 
             $data['rating'] = intval($data['rating']);
-
-            if (!empty($request->get("content"))) $data['content'] = $request->get("content");
 
             if (Str::length($data['content']) > 255) return $this->sendError("Content must be <= 255 characters", 400);
 
